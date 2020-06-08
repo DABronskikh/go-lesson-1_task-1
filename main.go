8d9a2e02eccfb81538ca5c65f9ebed6d2040a6ea
bronskih <dima5202@mail.ru>
2020-06-08 20:45:48 +0500

    Обновление формулы для расчета суммы bonus

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 func main() {
 	purchase := 3333_33
 	percent := 1
-	limit := 100 * 100
+	limit := 100
 
-	bonus := purchase * percent / 100
+	bonus := purchase * percent / (100 * 100)
 	if bonus > limit {
 		bonus = limit
 	}
